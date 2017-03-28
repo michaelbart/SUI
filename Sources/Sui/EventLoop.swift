db1948995238d48c1aaa0e3a6f8347b6b3e4a8be
Micah Bunting <Amnykon@gmail.com>
2017-03-27 21:54:41 -0500

    Add EventLoop

---
+++ Sources/Sui/EventLoop.swift
@@ -0,0 +1,31 @@
+import CoreFoundation
+
+/**
+  Starts the EventLoop in a run cycle and block until eventloopStop is called from an event.
+
+  eventLooprun may be called recursively. When calling recusivly, eventLoopStop will only unblock last called eventLoopRun.
+**/
+func eventLoopRun() {
+  CFRunLoopRun()
+}
+
+/**
+  stops the EventLoop from a run cycle. The event loop may continue until there are no closures to be ran.
+
+  If eventLoopRan was called recusivly, eventLoopStop will only unblock last called eventLoopRun.
+**/
+func eventLoopStop() {
+  eventLoopPerform{
+    CFRunLoopStop(CFRunLoopGetCurrent())
+  }
+  CFRunLoopWakeUp(CFRunLoopGetCurrent())
+}
+
+/**
+  Adds an closure to be ran on the EventLoop.
+*/
+func eventLoopPerform(_ block: @escaping ()->()) {
+  CFRunLoopPerformBlock(CFRunLoopGetCurrent(), kCFRunLoopDefaultMode, block) 
+  CFRunLoopWakeUp(CFRunLoopGetCurrent())
+}
+
